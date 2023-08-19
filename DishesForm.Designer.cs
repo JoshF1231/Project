3536291bb83f3ab19e7d5ffa91bbf995599d6438
Kiril <bialkiril123@gmail.com>
2023-08-19 21:51:18 +0300

    Kiril

--- DishesForm.Designer.cs
+++ DishesForm.Designer.cs
@@ -29,34 +29,148 @@
         private void InitializeComponent()
         {
             Temporary = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // Temporary
             // 
             Temporary.AutoSize = true;
             Temporary.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            Temporary.Location = new Point(298, 208);
+            Temporary.Location = new Point(295, 22);
+            Temporary.Margin = new Padding(4, 0, 4, 0);
             Temporary.Name = "Temporary";
-            Temporary.Size = new Size(177, 27);
+            Temporary.Size = new Size(266, 41);
             Temporary.TabIndex = 0;
             Temporary.Text = "DISHES FORM";
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DimGray;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(204, 750);
+            panel1.TabIndex = 1;
+            panel1.Paint += panel1_Paint;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Brown;
+            panel2.Controls.Add(Temporary);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(204, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(939, 107);
+            panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("David", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(0, 113);
+            button1.Name = "button1";
+            button1.Size = new Size(204, 77);
+            button1.TabIndex = 3;
+            button1.Text = "Dairy";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("David", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(0, 196);
+            button2.Name = "button2";
+            button2.Size = new Size(204, 77);
+            button2.TabIndex = 4;
+            button2.Text = "MeatBased";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("David", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(0, 30);
+            button3.Name = "button3";
+            button3.Size = new Size(204, 77);
+            button3.TabIndex = 5;
+            button3.Text = "Home";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("David", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(474, 382);
+            button4.Name = "button4";
+            button4.Size = new Size(204, 77);
+            button4.TabIndex = 6;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("David", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(483, 526);
+            button5.Name = "button5";
+            button5.Size = new Size(204, 77);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
             // DishesForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
-            Controls.Add(Temporary);
+            ClientSize = new Size(1143, 750);
+            Controls.Add(button5);
+            Controls.Add(panel2);
+            Controls.Add(button4);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 5, 4, 5);
             Name = "DishesForm";
             Text = "DishesForm";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label Temporary;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
     }
 }
--- DishesForm.cs
+++ DishesForm.cs
@@ -16,5 +16,25 @@ namespace Project
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
